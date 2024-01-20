7b506aa6dc019ec253f4314e34d06f4e4886da57
Seth Heinzman <50162434+MichaelHeinzman@users.noreply.github.com>
2024-01-19 19:58:15 -0500

    Update page.tsx to comment out install_plugins batch file execution.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,13 @@ import Instructions from "@/components/home/instructions";
 import { checkIfHasPlugins, handleInstallPlugin } from "@/lib/handle_plugins";
 
 const Home = async () => {
-  let hasPlugins: { buttonMash: boolean; SOS: boolean } | undefined;
+  // let hasPlugins: { buttonMash: boolean; SOS: boolean } | undefined;
 
-  hasPlugins = await checkIfHasPlugins();
-  if (!hasPlugins.buttonMash || !hasPlugins.SOS) {
-    await handleInstallPlugin();
-    hasPlugins = await checkIfHasPlugins();
-  }
+  // hasPlugins = await checkIfHasPlugins();
+  // if (!hasPlugins.buttonMash || !hasPlugins.SOS) {
+  //   await handleInstallPlugin();
+  //   hasPlugins = await checkIfHasPlugins();
+  // }
 
   return (
     <main className="min-h-screen w-full bg-gray-900 p-3 rounded-sm flex flex-col gap-4">
